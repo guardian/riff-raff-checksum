58d59951613a5c10892985106d33a9616da65c66
Natasha <67543397+NovemberTang@users.noreply.github.com>
2023-07-18 12:39:45 +0100

    Fix action setup
    Co-authored-by: Thalia Silver <15648334+tjsilver@users.noreply.github.com>

--- aws.py
+++ aws.py
@@ -1,14 +1,12 @@
-import boto3
-s3 = boto3.client('s3')
 bucket = 'riffraff-artifact'
 
-def get_keys(project: str, build: int, bucket: str = bucket, s3Client = s3) -> list[str]:
+def get_keys(project: str, build: int, s3Client, bucket: str = bucket) -> list[str]:
   response = s3Client.list_objects_v2(Bucket=bucket, Prefix=f'{project}/{build}/')
   return sorted([f['Key'] for f in response['Contents']])
 
-def _get_etag(object_path: str, bucket: str = bucket,  s3Client = s3) -> str:
+def _get_etag(object_path: str, bucket, s3Client ) -> str:
   return s3Client.head_object(Bucket=bucket, Key=object_path)['ETag']
 
-def get_etags(object_names: list[str], bucket: str = bucket,  s3Client = s3):
+def get_etags(object_names: list[str], s3Client, bucket: str = bucket):
  return [{'key' : object_path, 'etag' : _get_etag(object_path, bucket, s3Client)}for object_path in object_names]
 
--- compare.py
+++ compare.py
@@ -1,6 +1,8 @@
 import sys
 from aws import *
 from error_checks import *
+import boto3
+import os
 
 project, b1, b2 = str(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
 
@@ -8,15 +10,24 @@ def _strip_prefixes(build: int, ls: list[str]) -> str:
   return [string.replace(f'{project}/{build}/', '') for string in ls]
 
 def compare(project: str, b1: int, b2: int):
+
+  s3Client = boto3.client(
+    's3',
+    'eu-west-1',
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+    aws_session_token=os.environ.get('AWS_SESSION_TOKEN'))
+  print("created s3 client")
+
   build_match_error(b1, b2)
 
-  b1_keys, b2_keys = get_keys(project, b1), get_keys(project, b2)
+  b1_keys, b2_keys = get_keys(project, b1, s3Client), get_keys(project, b2, s3Client)
   file_count_error(b1, b2, b1_keys, b2_keys) # Fail if builds do not contain the same number of files
 
   b1_files, b2_files = _strip_prefixes(b1, b1_keys), _strip_prefixes(b2, b2_keys)
   file_name_error(b1, b2, b1_files, b2_files) # Fail if file paths do not match
 
-  build_1_etags, build_2_etags = get_etags(b1_keys), get_etags(b2_keys)
+  build_1_etags, build_2_etags = get_etags(b1_keys, s3Client), get_etags(b2_keys, s3Client)
   checksum_error(build_1_etags, build_2_etags) # Fail if checksums do not match
   print('File names and checksums match')
 
